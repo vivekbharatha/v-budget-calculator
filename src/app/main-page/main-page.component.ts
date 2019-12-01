ce8bfc0c7b6d33db0225f275cafdacb7406b0e8b
vivekbharatha <vivek.bharatha@gmail.com>
2019-12-02 00:12:07 +0530

    adds localStorage support to save data

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -13,16 +13,33 @@ export class MainPageComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    let dbItems = localStorage.getItem('items') || '[]';
+    this.budgetItems = JSON.parse(dbItems);
+    this.refreshTotalBudget();
+  }
 
   addItem(newItem: BudgetItem) {
     this.budgetItems.push(newItem);
     this.totalBudget += newItem.amount;
+    this.saveToDB();
   }
 
   deleteItem(item: BudgetItem) {
     this.budgetItems.splice(this.budgetItems.indexOf(item),1);
     this.totalBudget -= item.amount;
+    this.saveToDB();
+  }
+
+  saveToDB() {
+    localStorage.setItem('items', JSON.stringify(this.budgetItems));
+  }
+
+  refreshTotalBudget() {
+    this.totalBudget = 0;
+    this.budgetItems.forEach((item) => {
+      this.totalBudget += item.amount;
+    });
   }
 
 }
